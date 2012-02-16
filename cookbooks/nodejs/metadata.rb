15023b6b2447013771761aca11f40c03650a0cce
andyhu1007 <andyhu1007@gmail.com>
2012-02-16 14:08:01 +0800

    Add dependency in metadata

--- cookbooks/nodejs/metadata.rb
+++ cookbooks/nodejs/metadata.rb
@@ -4,3 +4,7 @@ license          "All rights reserved"
 description      "Installs/Configures nodejs"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+recipe           "nodejs", "Installs Node.JS from source"
+recipe           "nodejs::npm", "Installs npm - a package manager for node"
+
+depends          "build-essential"
