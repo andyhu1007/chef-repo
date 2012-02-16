f770e5c536b2db778bc9644f99a6c62a263fe346
andyhu1007 <andyhu1007@gmail.com>
2012-02-16 15:22:23 +0800

    Update nodejs version

--- cookbooks/nodejs/attributes/default.rb
+++ cookbooks/nodejs/attributes/default.rb
@@ -1,4 +1,4 @@
-default.nodejs[:version] = "0.6.8"
-default.nodejs[:checksum] = "e6cbfc5ccdbe10128dbbd4dc7a88c154d80f8a39c3a8477092cf7d25eef78c9c"
+default.nodejs[:version] = "0.6.10"
+default.nodejs[:checksum] = "d1d060ab53c6079409403530009a2095036f19920aabd3a1c20542e0db586bd5"
 default.nodejs[:dir] = "/usr/local"
-default.nodejs[:npm] = "1.0.105"
+default.nodejs[:npm] = "1.1.1"
